b053cabd3aadfba2a5597c1e92aa3400e11db53f
vter07 <82448797+vter07@users.noreply.github.com>
2021-07-20 16:28:40 +0900

    Update BaekJoon_1110.java

--- GitHub/BaekJoon_1110.java
+++ GitHub/BaekJoon_1110.java
@@ -3,7 +3,21 @@ package GitHub;
 public class BaekJoon_1110 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int oldNum = Integer.parseInt(sc.nextLine());
+		int count = 0;
+		int newNum = oldNum;
+		
+		while(true) {
+			int sum = 0;
+			count++; 
+			newNum = (newNum%10)*10+((newNum/10)+(newNum%10))%10;
+			
+			if(oldNum == newNum) {
+				System.out.println(count);
+				break;
+			} 
+	    	}
 
 	}
 

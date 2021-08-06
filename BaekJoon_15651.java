504973e04b543ae1065369da879010de1bd93a25
vter07 <82448797+vter07@users.noreply.github.com>
2021-08-06 14:53:37 +0900

    Add files via upload

---
+++ BaekJoon_15651.java
@@ -0,0 +1,37 @@
+package Main.copy;
+
+import java.util.*;
+import java.io.*;
+
+public class BaekJoon_15651 {
+	
+	static int[] arr;
+	static int n, m;
+	static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	
+	public static void main(String[] args) throws Exception {
+		Scanner sc = new Scanner(System.in);
+		
+		n = sc.nextInt();
+		m = sc.nextInt();
+		
+		arr = new int[m];
+		dfs(0);
+		bw.close();
+
+	}
+	
+	static void dfs(int depth) throws Exception {
+		if(m==depth) {
+			for(int val : arr) {
+				bw.write(val + " ");
+			}
+			bw.write("\n");
+			return;
+		}
+		for(int i = 0; i<n; i++) {
+			arr[depth] = i+1;
+			dfs(depth +1);
+		}
+	}
+}
